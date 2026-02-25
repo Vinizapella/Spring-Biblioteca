33c1b56b1ce509f0fbff8a0451d7b28c47a4c202
VINICIUS DOS SANTOS ZAPELLA <vinicius_zapella@estudante.sesisenai.org.br>
2026-02-24 21:13:43 -0300

    feat: finaliza os mappers

--- src/main/java/com/weg/biblioteca/mapper/EmprestimoMapper.java
+++ src/main/java/com/weg/biblioteca/mapper/EmprestimoMapper.java
@@ -1,4 +1,33 @@
 package com.weg.biblioteca.mapper;
 
+import com.weg.biblioteca.dto.emprestimo.EmprestimoRequestDto;
+import com.weg.biblioteca.dto.emprestimo.EmprestimoResponseDto;
+import com.weg.biblioteca.dto.usuario.UsuarioResponseDto;
+import com.weg.biblioteca.model.Emprestimo;
+import org.springframework.stereotype.Component;
+
+@Component
 public class EmprestimoMapper {
+
+    public Emprestimo toEntity(
+            EmprestimoRequestDto emprestimoRequestDto
+    ){
+        return new Emprestimo(
+                emprestimoRequestDto.id(),
+                emprestimoRequestDto.livro_id(),
+                emprestimoRequestDto.usuario_id(),
+                emprestimoRequestDto.data_emprestimo(),
+                emprestimoRequestDto.data_devolucao()
+        );
+    }
+
+    public EmprestimoResponseDto toResponse(Emprestimo emprestimo){
+        return new EmprestimoResponseDto(
+                emprestimo.getId(),
+                emprestimo.getLivro_id(),
+                emprestimo.getUsuario_id(),
+                emprestimo.getData_emprestimo(),
+                emprestimo.getData_devolucao()
+        );
+    }
 }
--- src/main/java/com/weg/biblioteca/mapper/LivroMapper.java
+++ src/main/java/com/weg/biblioteca/mapper/LivroMapper.java
@@ -1,4 +1,31 @@
 package com.weg.biblioteca.mapper;
 
+import com.weg.biblioteca.dto.livro.LivroRequestDto;
+import com.weg.biblioteca.dto.livro.LivroResponseDto;
+import com.weg.biblioteca.model.Livro;
+import org.springframework.stereotype.Component;
+
+@Component
 public class LivroMapper {
+
+    public Livro toEntit(
+            LivroRequestDto livroRequestDTO
+    ){
+        return new Livro(
+                livroRequestDTO.titulo(),
+                livroRequestDTO.autor(),
+                livroRequestDTO.ano_publicacao()
+        );
+    }
+
+    public LivroResponseDto toResponse(
+            Livro livro
+    ){
+        return new LivroResponseDto(
+                livro.getId(),
+                livro.getTitulo(),
+                livro.getAutor(),
+                livro.getAno_publicacao()
+        );
+    }
 }
