2a376a8dda0322512e86e15f341b2d6a2b9e40d1
VINICIUS DOS SANTOS ZAPELLA <vinicius_zapella@estudante.sesisenai.org.br>
2026-03-06 18:48:51 -0300

    feat: arruma a validacao do emprestimo, terminando a atividade

--- src/main/java/com/weg/biblioteca/dto/emprestimo/EmprestimoRequestDto.java
+++ src/main/java/com/weg/biblioteca/dto/emprestimo/EmprestimoRequestDto.java
@@ -1,12 +1,24 @@
 package com.weg.biblioteca.dto.emprestimo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+
 import java.util.Date;
 
 public record EmprestimoRequestDto (
-        long id,
+        @NotNull(message = "O id do livro nao pode ser nulo")
         long livro_id,
+        @NotNull(message = "O id do usuario nao pode ser nulo")
         long usuario_id,
+        @NotNull(message = "Data de empréstimo é obrigatória")
+        @PastOrPresent(message = "A data de empréstimo não pode ser futura")
+        @JsonFormat(pattern = "yyyy-MM-dd")
         Date data_emprestimo,
+        @NotNull(message = "Data de devolução é obrigatória")
+        @FutureOrPresent(message = "A data de devolução não pode ser anterior a hoje")
+        @JsonFormat(pattern = "yyyy-MM-dd")
         Date data_devolucao
 ){
 }
