b2eed0dedf7685c5b4437ee91881aa6fcab20cf5
VINICIUS DOS SANTOS ZAPELLA <vinicius_zapella@estudante.sesisenai.org.br>
2026-03-06 18:35:26 -0300

    feat: arruma validacao no usuarioDto

--- src/main/java/com/weg/biblioteca/dto/usuario/UsuarioRequestDto.java
+++ src/main/java/com/weg/biblioteca/dto/usuario/UsuarioRequestDto.java
@@ -1,7 +1,15 @@
 package com.weg.biblioteca.dto.usuario;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record UsuarioRequestDto (
+        @NotBlank(message = "O nome é obrigatorio")
+        @Size(min = 20, max = 50, message = "O nome deve ter no minimo de 3 caracteres e no maximo 50 caracteres")
         String nome,
+        @NotBlank(message = "Email é obrigatorio")
+        @Email(message = "Email invalido")
         String email
 ){
 }
